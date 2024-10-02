75827e79c05f8dd633fe0a1d62cc2cc070f2c01c
jorojojojo23 <jbarres@uc.cl>
2024-10-02 00:57:15 -0300

    App.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { AuthProvider } from './components/context/AuthProvider';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
