074b75d888dc3c4772a07579a2ac64d976055574
Martin Vial <martinviald@uc.cl>
2024-10-02 23:47:53 -0300

    port 3000

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3000";
 
 export default axios.create({
   baseURL: BASE_URL,
